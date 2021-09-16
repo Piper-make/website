b7d1cb60c15afa42d4eaf6b7371f0c3e263eadab
Louis Cutner <lcutner@me.com>
2021-09-16 14:28:16 +0100

    added creative mode page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,9 @@ module.exports = {
         top_bar: "url('images/top_bar.svg')",
         bottom_bar: "url('images/bottom_bar.svg')",
         background_image: "url('images/background.svg')",
+        background_image_creative_mode: "url('images/background-cm.svg')",
         option_1_name_bg: "url('images/option-1-name/bg.svg')",
+        text_bg_creative_mode: "url('images/cm-options/text-bg.svg')",
       }),
       transitionProperty: {
         height: "max-height",
